d33dbbad5ffdbbebbf9fa8576407063869afe6e1
Sebastian Amariles <sebas932@gmail.com>
2018-07-31 16:18:44 -0500

    Improving the interface

--- src/routes/application.php
+++ src/routes/application.php
@@ -10,35 +10,61 @@ $app->get('/', function ($request, $response, $args) {
 })->setName('index');
 
 $app->get('/{organization}/{repo}', function ($request, $response, $args) {
+  ini_set('max_execution_time', 300);
 
   $GH_URL = 'https://api.github.com';
   $org = $request->getAttribute('organization');
   $repo = $request->getAttribute('repo');
   $repoURL = $GH_URL.'/repos/'.$org.'/'.$repo;
   $milestoneID = $request->getQueryParam('milestoneID');
+  $zenhubActive = $request->getQueryParam('zenhubActive') == "true";
 
-  $repoInfo = api_request($repoURL);
-  $milestones = api_request($repoURL.'/milestones');
+  $repoInfo = githubRequest($repoURL);
+  $milestones = githubRequest($repoURL.'/milestones');
 
+  $allIssues = array();
+  $stopRequest = false;
+  $page = 1;
+  $perPage = 100;
 
-  if($milestoneID == ""){
-    $issues = api_request($repoURL.'/issues?state=all&per_page=100');
-  }else{
-    $issues = api_request($repoURL.'/issues?state=all&per_page=100&milestone='.$milestoneID);
+  // Get all issues from Github
+  do {
+    if(($milestoneID != "")){
+      $issues = githubRequest($repoURL.'/issues?state=all&page='.$page.'&per_page='.$perPage.'&milestone='.$milestoneID);
+    }else{
+      $issues = githubRequest($repoURL.'/issues?state=all&page='.$page.'&per_page='.$perPage);
+    }
+    if(count($issues) < $perPage){
+      $stopRequest = true;
+    }else{
+      $page = $page + 1;
+    }
+    $allIssues =  array_merge($allIssues, $issues);
+} while ($stopRequest == false);
+
+  // Get Issues information from Zenhub
+  if($zenhubActive){
+    $issuesZenhub = array();
+    foreach ($allIssues as $issue) {
+        $issue['zenhub'] = zenhubRequest('https://api.zenhub.io/p1/repositories/'.$repoInfo['id'].'/issues/'.$issue['number']);
+        $issuesZenhub[] = $issue;
+    }
+    $allIssues = $issuesZenhub;
   }
-
 
   return $this->view->render($response, 'repo.html', [
     'repoInfo' => $repoInfo,
     'milestones' => $milestones,
-    'issues' => $issues,
-    'milestoneID' => $milestoneID
+    'issues' => $allIssues,
+    'milestoneID' => $milestoneID,
+    'pages' => $page,
+    'zenhubActive' => $zenhubActive
   ]);
 })->setName('repo');
 
 
-// Github REST API example
-function api_request($url){
+// Github REST API
+function githubRequest($url){
     global $settings;
     $ch = curl_init();
     // Basic Authentication with token
@@ -49,6 +75,30 @@ function api_request($url){
 
     curl_setopt($ch, CURLOPT_URL, $url);
     //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/xml'));
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Agent smith');
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_USERPWD, $access);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    $result = json_decode(trim($output), true);
+    return $result;
+}
+
+// ZenHubIO/API REST API
+function zenhubRequest($url){
+    global $settings;
+    $ch = curl_init();
+    // Basic Authentication with token
+    // https://github.com/ZenHubIO/API
+    // curl -H 'X-Authentication-Token: TOKEN' https://api.zenhub.io/p1/repositories/:repo_id/issues/:issue_id
+    $access = $settings['zenhub']['token'];
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Authentication-Token: '.$access));
     curl_setopt($ch, CURLOPT_USERAGENT, 'Agent smith');
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_USERPWD, $access);
