ef933f161ecad9986fdce30b14bfb89f9ec94411
Sebastian Amariles <sebas932@gmail.com>
2019-04-21 23:37:08 -0500

    Added HTTP Headers

--- src/routes/api/api.php
+++ src/routes/api/api.php
@@ -2,6 +2,17 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+    ->withHeader('Access-Control-Allow-Origin', '*')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 $app->get('/api/{organization}/{repo}/milestones', function ($request, $response, $args) {
   // Managers
   $githubService = new \services\GithubService();
@@ -41,6 +52,5 @@ $app->get('/api/{organization}/{repo}/sprint/{milestoneID}', function ($request,
 
   return $response->withStatus(200)
         ->withHeader('Content-Type', 'application/json')
-        ->withHeader('Access-Control-Allow-Origin', '*')
         ->write(json_encode($output));
 });
